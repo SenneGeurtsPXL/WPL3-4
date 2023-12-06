b85f311ca29ba8e8e6e640c2d408e4bbd3598095
Maurizio Maniscalco <maurizio.maniscalco@student.pxl.be>
2023-12-06 14:55:08 +0100

    made test class "testMauri" , made folders for structure and made a driver maanger class to switch between drivers

---
+++ B-tube test automation framework/B-tube test automation framework/TestMauri.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using B_tube_test_automation_framework.Web_applicatie.Driver_Manager;
+
+namespace B_tube_test_automation_framework
+{
+    public class TestMauri
+    {
+        //aanmaken webdriver
+        //setup van driver
+
+        [Test]
+        public static void HardCodedTest()
+        {
+                var browser = DriverManager.Chrome;
+                browser.Navigate().GoToUrl("https://btube-app.onrender.com/#/");
+
+                browser.Navigate().Refresh();
+                browser.Manage().Window.Maximize();
+
+                WebDriverWait wait = new WebDriverWait(browser, TimeSpan.FromSeconds(60));
+
+                browser.Navigate().Refresh();
+                Thread.Sleep(5000);
+
+
+                IWebElement signInButton = wait.Until(ExpectedConditions.ElementExists(By.Id("SignInButton")));
+                signInButton.Click();
+
+
+                IWebElement userName = wait.Until(ExpectedConditions.ElementExists(By.Id("SignInEmail")));
+                IWebElement paswoord = wait.Until(ExpectedConditions.ElementExists(By.Id("SignInPassword")));
+
+
+                userName.SendKeys("stageadmin@stageadmin.stageadmin");
+                paswoord.SendKeys("StageAdmin0221!");
+
+                IWebElement signInButtonComplete = wait.Until(ExpectedConditions.ElementExists(By.Id("SignInButtonComplete")));
+                signInButtonComplete.Click();
+
+                Thread.Sleep(10000);
+
+
+                browser.Quit();
+            }
+
+
+            
+
+        }
+    }
+}
---
+++ B-tube test automation framework/B-tube test automation framework/Web applicatie/Driver Manager/DriverManager.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_tube_test_automation_framework.Web_applicatie.Driver_Manager
+{
+    public enum BrowserType
+    {
+        Chrome,
+        Edge
+    }
+
+    public static class DriverManager
+    {
+        public static IWebDriver Chrome
+        {
+            get
+            {
+                return new ChromeDriver();
+            }
+        }
+
+        public static IWebDriver Edge
+        {
+            get
+            {
+                return new ChromeDriver();
+            }
+        }
+    }
+
+
+}
