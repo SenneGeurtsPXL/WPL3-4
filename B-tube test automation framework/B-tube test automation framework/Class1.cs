ed81336140f4b10897ecb38743d2eff8d7bf02b8
Maurizio Maniscalco <maurizio.maniscalco@student.pxl.be>
2023-12-06 11:32:05 +0100

    C# project toevoegd

---
+++ B-tube test automation framework/B-tube test automation framework/Class1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_tube_test_automation_framework
+{
+    public class Class1
+    {
+        //aanmaken webdriver
+        public IWebDriver driver;
+        //setup van driver
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+        //test die wordt uitgevoerd
+        [Test]
+        public void hardCodedTest()
+        {
+            driver.Url = "https://btube-app.onrender.com/#/";
+        }
+        //afsluiten van de test
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
