36940321830c4f14fa947007af669a3e1ed40277
EmirhanAydemirPXL <95099457+EmirhanAydemirPXL@users.noreply.github.com>
2023-12-06 14:17:05 +0100

    Eerste Hardgecodeerde test

--- B-tube test automation framework/B-tube test automation framework/test.cs
+++ B-tube test automation framework/B-tube test automation framework/test.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
+using System.Threading;
 
 namespace B_tube_test_automation_framework
 {
@@ -18,25 +19,36 @@ namespace B_tube_test_automation_framework
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://btube-app.onrender.com/#/");
-            driver.Manage().Window.Maximize();
+            
             driver.Navigate().Refresh();
+            driver.Manage().Window.Maximize();
+
 
             
-            
-
             WebDriverWait wait = new WebDriverWait (driver, TimeSpan.FromSeconds(60));
-            IWebElement signInButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("RegisterButton")));
 
+            driver.Navigate().Refresh();
+            Thread.Sleep(10000);
+
+
+
+            IWebElement signInButton = wait.Until(ExpectedConditions.ElementExists(By.Id("SignInButton")));
             signInButton.Click();
 
+
+
+
             IWebElement userName = wait.Until(ExpectedConditions.ElementExists(By.Id ("SignInEmail")));
-            IWebElement paswoord = wait.Until(ExpectedConditions.ElementExists(By.Id ("SignInPasswoord")));
+            IWebElement paswoord = wait.Until(ExpectedConditions.ElementExists(By.Id ("SignInPassword")));
 
 
             userName.SendKeys("stageadmin@stageadmin.stageadmin");
             paswoord.SendKeys("StageAdmin0221!");
+            
+            IWebElement signInButtonComplete = wait.Until(ExpectedConditions.ElementExists(By.Id("SignInButtonComplete")));
+            signInButtonComplete.Click();
 
-
+            Thread.Sleep(10000);
 
 
 
